d7c9ff96023f8726ce8cfc48d659faf418f5d888
Nicolas Perriault <nicolas@perriault.net>
2018-08-28 12:31:25 +0200

    Add reproducible crash case.

--- skeleton/src/Views/Page.elm
+++ skeleton/src/Views/Page.elm
@@ -1,11 +1,9 @@
 module Views.Page exposing (ActivePage(..), Config, frame)
 
 import Browser exposing (Document)
-import Css exposing (..)
 import Data.Session exposing (Session)
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (class, css, href, src)
-import Views.Theme exposing (Element, defaultCss)
+import Html.Styled.Attributes exposing (class)
 
 
 type ActivePage
@@ -20,61 +18,15 @@ type alias Config =
 
 
 frame : Config -> Html msg -> Document msg
-frame config content =
-    { title = "Page.frame"
+frame _ content =
+    { title = "test"
     , body =
-        [ div []
-            [ defaultCss
-            , viewHeader config
-            , div [ css [ padding2 (Css.em 1) zero ] ] [ content ]
-            ]
-            |> toUnstyled
+        [ h1 [] [ text "Crash test" ] |> toUnstyled
+
+        -- This works
+        , div [] [] |> toUnstyled
+
+        -- This crashes the runtime
+        , div [ class "plop" ] [] |> toUnstyled
         ]
     }
-
-
-githubIconStyle : Element msg
-githubIconStyle =
-    styled a
-        [ position absolute
-        , top (px 15)
-        , right (px 15)
-        , border3 (px 1) solid (rgba 255 255 255 0.3)
-        , padding (px 10)
-        , borderRadius (px 4)
-        , color (hex "999")
-        , textDecoration none
-        ]
-
-
-title : Element msg
-title =
-    styled h1
-        [ textAlign center
-        , margin2 (Css.em 1) zero
-        , color (hex "000")
-        , fontSize (px 60)
-        , lineHeight (px 1)
-        ]
-
-
-viewHeader : Config -> Html msg
-viewHeader _ =
-    div [ class "header" ]
-        [ title [] [ text "elm-kitchen" ]
-        , githubIconStyle
-            [ Html.Styled.Attributes.target "_blank"
-            , href "https://github.com/allo-media/elm-kitchen"
-            ]
-            [ img
-                [ src "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ei-sc-github.svg/768px-Ei-sc-github.svg.png"
-                , css
-                    [ width (px 96)
-                    , height (px 96)
-                    , float left
-                    ]
-                ]
-                []
-            , span [ css [ color (hex "000"), display block, textAlign center ] ] [ text "Github" ]
-            ]
-        ]
